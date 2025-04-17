2ffb42bf991c0254d4119960c2c0725c7bcbceaf
Andrzej Machnik <andrzejm.edu@pm.me>
2025-04-17 07:27:11 +0000

    Added Get-ChildOUs.ps1
    Script that lists child Organizational Units of provided Parent OUs. Can be used with Export-UserInfo.ps1 for easier input of desired OUs.
    Created at: 17.04.2025

---
+++ Get-ChildOUs.ps1
@@ -0,0 +1,25 @@
+﻿param (
+    [Parameter(Mandatory= $true)]
+    [string[]]$ParentOUs
+)
+
+$childOUList = @()
+
+foreach($parentOUName in $ParentOUs) {
+    # znajdz pelna nazwe DN jednostki-rodzica
+    $currentParent = Get-ADOrganizationalUnit -Filter "Name -eq '$parentOUName'" -Properties DistinguishedName
+
+    if(-not $currentParent) {
+        Write-Warning "Nie znaleziono jednostki organizacyjnej '$parentOUName'"
+        continue
+    }
+
+    foreach($parentOU in $currentParent) {
+        # znajdz wszystkie jednostki-dzieci
+        $childOUs = Get-ADOrganizationalUnit -SearchBase $parentOU.DistinguishedName -SearchScope Subtree -Filter * | Select-Object -ExpandProperty Name
+        $childOUList += $childOUs
+    }
+}
+
+# usun duplikaty, wyswietl wynik (mozna przekazac do drugiego skryptu)
+$childOUList | Sort-Object -Unique
